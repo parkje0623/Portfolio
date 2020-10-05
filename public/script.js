f977d4c6ded55ade15d4c13d6184b7c0c80cbdd0
Jieung Park <parkje0623@gmail.com>
2020-10-04 23:59:25 -0700

    about finished

--- public/script.js
+++ public/script.js
@@ -3,10 +3,10 @@ const name = document.querySelector('.navbar_name');
 const menu = document.querySelector('.navbar_menu');
 
 const about = document.getElementById('aboutBtn');
-const exp = document.getElementById('expBtn');
 const port = document.getElementById('portBtn');
 const contact = document.getElementById('contactBtn');
 
+/* Setup for fade-in effect when scrolling */
 var trigger = new ScrollTrigger.default();
 trigger.add('[data-trigger]', {
   once: false,
@@ -17,36 +17,27 @@ trigger.add('[data-trigger]', {
   addHeight: true
 });
 
+/* When menu icon clicked, show entire menu list */
 menuIcon.addEventListener('click', () => {
   name.classList.toggle('active');
   menu.classList.toggle('active');
 });
 
+/* Clicking each About, Exp, Port, Contact will decrease opacity of others and increase opacity of clicked menu */
 function clickAbout() {
   about.style.opacity = "1";
-  exp.style.opacity = "0.5";
-  port.style.opacity = "0.5";
-  contact.style.opacity = "0.5";
-  document.querySelector('.about_paragraph').classList.toggle('active');
-}
-
-function clickExp() {
-  about.style.opacity = "0.5";
-  exp.style.opacity = "1";
   port.style.opacity = "0.5";
   contact.style.opacity = "0.5";
 }
 
 function clickPort() {
   about.style.opacity = "0.5";
-  exp.style.opacity = "0.5";
   port.style.opacity = "1";
   contact.style.opacity = "0.5";
 }
 
 function clickContact() {
   about.style.opacity = "0.5";
-  exp.style.opacity = "0.5";
   port.style.opacity = "0.5";
   contact.style.opacity = "1";
 }
